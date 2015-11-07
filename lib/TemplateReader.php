2fe070e4e4d7060f7a6059fa7850213f0503cf58
Ievgen Kuzminov <kyzminov@gmail.com>
2015-11-07 17:44:18 +0100

    save sections

--- lib/TemplateReader.php
+++ lib/TemplateReader.php
@@ -15,4 +15,22 @@ class TemplateReader
         $path = \Yii::getAlias('@webroot') . "/templates/{$tplId}/sections/{$sectionName}.html";
         return file_get_contents($path);
     }
+
+    static function getSectionTplId($sectionName)
+    {
+        switch ($sectionName) {
+            case 'heading':
+                return 1;
+                break;
+            case 'contacts':
+                return 4;
+                break;
+            case 'services':
+                return 2;
+                break;
+            case 'gallery':
+                return 3;
+                break;
+        }
+    }
 }
