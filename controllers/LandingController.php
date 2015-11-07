a038d383e13148eb86493a1eb1dc7baa2db45615
Sergey Koba <s.koba@mobidev.biz>
2015-11-07 15:40:32 +0200

    landing controller auth rules

--- controllers/LandingController.php
+++ controllers/LandingController.php
@@ -2,8 +2,34 @@
 
 namespace app\controllers;
 
-class LandingController extends \yii\web\Controller
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+
+class LandingController extends Controller
 {
+    public function behaviors()
+    {
+        return array_merge_recursive(parent::behaviors(), [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['edit', 'create', 'delete'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ]
+        ]);
+    }
+
     public function actionShow()
     {
         return $this->render('show');
