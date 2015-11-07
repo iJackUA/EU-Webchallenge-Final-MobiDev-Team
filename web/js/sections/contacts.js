2506aecc76c03aedeff3c917d2b302a8713ef227
Ievgen Kuzminov <kyzminov@gmail.com>
2015-11-07 15:54:59 +0100

    landing editor v2

--- web/js/sections/contacts.js
+++ web/js/sections/contacts.js
@@ -1,1 +1,13 @@
+var g = require('../global');
+
+module.exports = {
+    template: window.gon.templates['contacts'](),
+
+    props: ['s'],
+
+    data: function () {
+        return {}
+    },
+    methods: {}
+}
 
--- web/js/sections/gallery.js
+++ web/js/sections/gallery.js
@@ -1,1 +1,14 @@
+var g = require('../global');
 
+module.exports = {
+    template: window.gon.templates['gallery'](),
+
+    props: ['s'],
+
+    data: function () {
+        return {}
+    },
+    methods: {
+
+    }
+}
